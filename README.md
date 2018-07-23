08c207ce33c35e239fed4bbce915e6b75f42daae
DESKTOP-R3OHP7R\Jack <akdodji@gmail.com>
2018-07-23 17:25:16 -0400

    Updating the readme file

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 ## [Portfolio](https://github.com/JackEd0/jack)
+<img src="https://i.imgur.com/UOZEjQP.png" alt="Logo" width="200px"/>
 
 Portfolio is web interface that allow anyone to generate a portfolio template.
 
-**Example**: <https://www.jack.webtech.center/>
+**Curent version:** 1.1.0
 
-Curent version: 1.1.0
+<img src="https://i.imgur.com/zIPCDfB.jpg" alt="Screenshot1" width="400px"/>
+<img src="https://i.imgur.com/RfmScvD.jpg" alt="Screenshot2" width="400px"/>
 
+### Installing
+
+ - Place folder on a PHP server, the root file is index.php
+ - Add your resume to the folder 'assets/pdf'
+ - Create a file named 'user.php' and place it in the 'includes' folder
+ - In 'user.php', rename the 'resume_file' with the name of your resume file. See 'user.example.php'
 
 ### Features
 
@@ -30,6 +38,10 @@ Curent version: 1.1.0
 
 ### Notes
 
-Started: 30 June 2017
-Last updated: 30 June 2017
-13hrs of work
+Started: 30 June 2017.
+Last updated: 23 July 2018.
+83hrs of work.
+
+### Licensing
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](https://opensource.org/licenses/MIT)
---
+++ includes/user.example.php
@@ -0,0 +1,74 @@
+<?php
+return array(
+    'full_name' => 'John Smith',
+    'name' => 'John',
+    'profession_en' => 'Full stack developer',
+    'profession_fr' => 'Développeur full stack',
+    'email' => 'example@gmail.com',
+    'default_lang' => 'en',
+    'resume_file' => '/pdf/resume-example.pdf',
+    'about_en' => 'I am passionate about Technology and speak English and French. <br>
+        Do not hesitate to contact me.',
+    'about_fr' => 'Je suis passionné de technologie et parle l\'anglais et le français. <br>
+        N\'hésitez pas à me contacter.',
+    'description_en' => '<p>I am graduating in Bachelor of Computer Engineering at
+        <a href="https://www.uexample.ca/">Université de Example</a> .</p>
+        <p>My latest job is: PHP developer at <a href="https://www.companywebsite.com/">Company Example</a>.</p>
+        <p>My job is to create WordPress plugins and add new contents to old sites.</p>',
+    'description_fr' => '<p>Je suis diplômé en Baccalauréat de Génie Informatique à
+        <a href="https://www.uexample.ca/">l\'Université de Example</a> .</p>
+        <p>Mon dernier travail est: développeur PHP à <a href="https://www.companywebsite.com/">Plogg Media</a>.</p>
+        <p>Mon travail consiste à créer des plugins WordPress et à ajouter de nouveaux contenus à d\'anciens sites.</p>',
+    'realisations' => [
+        [
+            'title' => 'Realisation 1',
+            'preview' => 'one-page-crud.jpg',
+            'link' => 'https://www.example1.center',
+            'description' => 'Single Page to CRUD entities'
+        ],
+        [
+            'title' => 'Realisation 2',
+            'preview' => 'bd.jpg',
+            'link' => 'https://www.example2.com',
+            'description' => 'Site de vente de documents'
+        ],
+        [
+            'title' => 'PORTFOLIO',
+            'preview' => 'portfolio.jpg',
+            'link' => 'https://www.example1.center',
+            'description' => 'Site servant de portfolio'
+        ],
+        [
+            'title' => 'Waspnode',
+            'preview' => 'waspnode.jpg',
+            'link' => 'https://example1.htm',
+            'description' => 'Site de presentation'
+        ],
+        [
+            'title' => 'Nemaxe',
+            'preview' => 'nemaxe.jpg',
+            'link' => 'https://www.example1.center/',
+            'description' => 'Site de creation d\'épreuves'
+        ],
+    ],
+    'linkedin' => 'https://ca.linkedin.com/in/john-smith-3816582c151',
+    'github' => 'https://github.com/JohnSmith',
+    'main_language' => 'PHP',
+    'main_skills' => [
+        'PHP', 'Wordpress', 'HTML5', 'MySQL', 'Laravel'
+    ],
+    'skills' => [
+        [
+            'JAVA', 'JSF', 'PYTHON', 'CSS', 'C++', 'JAVASCRIPT', 'AJAX', 'SQL', 'ASSEMBLEUR X86'
+        ],
+        [
+            'BOOTSTRAP', 'JOOMLA', 'WIX'
+        ],
+        [
+            'BLENDER 3D', 'MATLAB', 'ALTIUM', 'QT CREATOR', 'VISUAL STUDIO', 'WINDEV', 'SUITE OFFICE'
+        ],
+        [
+            'FPGA', 'RASPBERRY PI', 'KALI', 'UBUNTU', 'WIN 7/8/10', 'MBED'
+        ]
+    ],
+);
